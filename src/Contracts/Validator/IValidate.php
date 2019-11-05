178eae564271253ae76d174791cedc8adfa698b0
沧澜 <lirongyi@51ucar.cn>
2019-11-05 14:34:46 +0800

    laravel productivity

---
+++ src/Contracts/Validator/IValidate.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Author: 沧澜
+ * Date: 2019-10-14
+ */
+
+namespace Calject\LaravelProductivity\Contracts\Validator;
+
+/**
+ * Interface IValidate
+ * @package Calject\LaravelProductivity\Contracts\Validator
+ */
+interface IValidate
+{
+    /**
+     * @return mixed
+     */
+    public function validate();
+}
---
+++ src/Providers/CommandsProvider.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Author: 沧澜
+ * Date: 2019-10-25
+ */
+
+namespace Calject\LaravelProductivity\Providers;
+
+use Calject\LaravelProductivity\Consoles\Commands\DataCommentCommand;
+use Calject\LaravelProductivity\Consoles\Commands\EnvConfigCommand;
+use Calject\LaravelProductivity\Consoles\Commands\ModelCommentCommand;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class CommandsProvider
+ * @package Calject\LaravelProductivity\Providers
+ */
+class CommandsProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                ModelCommentCommand::class,
+                DataCommentCommand::class,
+                EnvConfigCommand::class,
+            ]);
+        }
+    }
+    
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    
+    
+    }
+    
+}
